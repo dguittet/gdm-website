c6cf44d8b4db25c0db7143ae281ff4e3d6ffdbcb
Darice <darice.guittet@nrel.gov>
2026-01-23 11:48:09 -0700

    add build

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'NREL', // Usually your GitHub org/user name.
-  projectName: 'grid-data-models', // Usually your repo name.
+  url: 'https://dguittet.github.io',
+  baseUrl: '/gdm-website/',
+  organizationName: 'NLR',
+  projectName: 'gdm-website',
 
   onBrokenLinks: 'throw',
 
